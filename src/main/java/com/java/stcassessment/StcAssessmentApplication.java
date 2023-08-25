401916aa15588abfc055c59e68ecce15fa8bca8b
DESKTOP-UALOQG0\MMS <mahmoud.magdy@dentacarts.com>
2023-08-25 23:33:48 +0300

    Initial commit

---
+++ src/main/java/com/java/stcassessment/StcAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.java.stcassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/stcassessment/StcAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.stcassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
